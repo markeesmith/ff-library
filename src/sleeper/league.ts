dafd356e008ed03d8986d26e7902ef3721a238f7
Markee Smith <markee.j.smith@gmail.com>
2021-12-05 21:51:05 -0500

    Added base league code and tests

---
+++ src/sleeper/league.ts
@@ -0,0 +1,77 @@
+import { requestJson } from "../utils/request";
+
+import {
+    LeagueInfo,
+    LeagueRosters,
+    LeagueUsers,
+    LeagueMatchups,
+    LeagueWinnersBracket,
+    LeagueTransactions,
+    LeagueTradedPicks,
+} from "../../types/LeagueTypes";
+import { DraftForLeague } from "../../types/DraftTypes";
+
+async function getLeagueInfo(leagueId: string): Promise<LeagueInfo> {
+    return requestJson(`https://api.sleeper.app/v1/league/${leagueId}`);
+}
+
+async function getLeagueRosters(
+    leagueId: string
+): Promise<Array<LeagueRosters>> {
+    return requestJson(`https://api.sleeper.app/v1/league/${leagueId}/rosters`);
+}
+
+async function getLeagueUsers(leagueId: string): Promise<Array<LeagueUsers>> {
+    return requestJson(`https://api.sleeper.app/v1/league/${leagueId}/users`);
+}
+
+async function getLeagueMatchupsByWeek(
+    leagueId: string,
+    week: number
+): Promise<Array<LeagueMatchups>> {
+    return requestJson(
+        `https://api.sleeper.app/v1/league/${leagueId}/matchups/${week}`
+    );
+}
+
+async function getLeagueWinnersBracket(
+    leagueId: string
+): Promise<Array<LeagueWinnersBracket>> {
+    return requestJson(
+        `https://api.sleeper.app/v1/league/${leagueId}/winners_bracket`
+    );
+}
+
+async function getLeagueTransationsByWeek(
+    leagueId: string,
+    week: number
+): Promise<Array<LeagueTransactions>> {
+    return requestJson(
+        `https://api.sleeper.app/v1/league/${leagueId}/transactions/${week}`
+    );
+}
+
+async function getLeagueTradedPicks(
+    leagueId: string
+): Promise<Array<LeagueTradedPicks>> {
+    return requestJson(
+        `https://api.sleeper.app/v1/league/${leagueId}/traded_picks`
+    );
+}
+
+async function getLeagueDrafts(
+    leagueId: string
+): Promise<Array<DraftForLeague>> {
+    return requestJson(`https://api.sleeper.app/v1/league/${leagueId}/drafts`);
+}
+
+export {
+    getLeagueInfo,
+    getLeagueRosters,
+    getLeagueUsers,
+    getLeagueMatchupsByWeek,
+    getLeagueWinnersBracket,
+    getLeagueTransationsByWeek,
+    getLeagueTradedPicks,
+    getLeagueDrafts,
+};
