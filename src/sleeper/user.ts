80fca8c255763a0945cbbacca959463e84303168
Markee Smith <markeesmith@Markees-MacBook-Pro.local>
2023-04-06 14:30:55 -0400

    Reset repository

--- src/sleeper/user.ts
+++
@@ -1,8 +0,0 @@
-import { requestJson } from "../utils/request";
-import { UserInfo } from "../UserTypes";
-
-async function getUserInfo(userId: string): Promise<UserInfo> {
-    return requestJson<UserInfo>(`https://api.sleeper.app/v1/user/${userId}`);
-}
-
-export { getUserInfo };
