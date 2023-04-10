57123199757e0d116165ecef9f7ed20be79eeccb
Markee Smith <markee.j.smith@gmail.com>
2023-04-10 09:16:01 -0400

    Add Request class

---
+++ src/utils/request.ts
@@ -0,0 +1,69 @@
+import fetch, { Response } from "node-fetch"
+
+/**
+ * Send async request(s) and receive the response in the desired format
+ */
+class Request {
+    url: string;
+
+    constructor(props: RequestProps) {
+        this.url = props.requestUrl;
+    }
+
+    /**
+     * Get the full response from the provided request URL
+     * @returns A promise that contains the full response
+     */
+    async getFullResponse(): Promise<Response> {
+        return fetch(this.url)
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error(response.statusText);
+                }
+                return response;
+            })
+    }
+
+    /**
+     * Get the response payload in JSON format
+     * @returns A promise that contains the response payload in JSON format
+     */
+    async getResponseJson<T>(): Promise<T> {
+        return fetch(this.url)
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error(response.statusText);
+                }
+                return response.json() as Promise<T>;
+            })
+            .then((data) => {
+                return data;
+            })
+    }
+
+    /**
+     * Get the response status
+     * @returns A promise that contains the response status
+     */
+    async getResponseStatus(): Promise<number> {
+        return fetch(this.url)
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error(response.statusText);
+                }
+                return response.status;
+            });
+    }
+}
+
+interface RequestProps {
+    /**
+     * Full URL of request
+     */
+    requestUrl: string;
+}
+
+export {
+    Request,
+    RequestProps
+}
