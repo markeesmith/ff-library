426f4b69749635f313a3779451542034585f348e
Markee Smith <markee.j.smith@gmail.com>
2023-04-10 12:48:11 -0400

    Update dependencies for unit testing
    ** Overview of Changes **
    
    1. Added babel dependencies for building typescript-written jest tests
    2. Rolled back node-fetch from 3.3.x to 2.6.7 due to node-fetch issues
       when running unit tests

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    preset: "ts-jest",
+    testEnvironment: "node",
+    verbose: true,
+    collectCoverage: true,
+    rootDir: "./",
+    roots: ["<rootDir>"],
+    moduleDirectories: ["node_modules", "src"],
+    moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+    testMatch: ["**/tst/**/*.test.ts"],
+    transform: {
+        "^.+\\.(t|j)s?$": "babel-jest",
+    },
+    transformIgnorePatterns: [
+        "node_modules/(?!(node-fetch" + "|fetch-blob" + ")/)",
+    ]
+};
