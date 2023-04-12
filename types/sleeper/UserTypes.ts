9a7e0ac54f7e126b719fcd238b8313c9750f5967
Markee Smith <markee.j.smith@gmail.com>
2023-04-12 14:56:34 -0400

    Add UserTypes Types for Sleeper

---
+++ types/sleeper/UserTypes.ts
@@ -0,0 +1,25 @@
+interface UserInfo {
+    verification: unknown;
+    username: string;
+    user_id: string;
+    token: unknown;
+    summoner_region: unknown;
+    summoner_name: unknown;
+    solicitable: unknown;
+    real_name: unknown;
+    phone: string;
+    pending: unknown;
+    notifications: unknown;
+    metadata: unknown;
+    is_bot: boolean;
+    email: string;
+    display_name: string;
+    deleted: unknown;
+    data_updated: unknown;
+    currencies: unknown;
+    created: unknown;
+    cookies: unknown;
+    avatar: string;
+}
+
+export { UserInfo };
